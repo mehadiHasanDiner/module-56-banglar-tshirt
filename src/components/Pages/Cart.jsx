80903204f464610926f94d4d996789e9987087c4
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-10-10 17:18:28 +0600

    conditional rendering

--- src/components/Pages/Cart.jsx
+++ src/components/Pages/Cart.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
 const Cart = ({ cart, handleRemoveFromCart }) => {
-  console.log(cart);
+  // console.log(cart);
+  let message;
+  if (cart.length === 0) {
+    message = <p>Please add some product</p>;
+  }
   return (
     <div className="text-center">
-      <h2 className="font-bold"> Order Summary: {cart.length}</h2>
+      <h2 className="font-bold mb-4"> Order Summary: {cart.length}</h2>
+      {message}
+
       {cart.map((tshirt) => (
         <p
           key={tshirt._id}
@@ -24,3 +30,8 @@ const Cart = ({ cart, handleRemoveFromCart }) => {
 };
 
 export default Cart;
+
+/**
+ * CONDITIONAL RENDERING
+ * 1. use if else to set a variable that will contain an element, components
+ * */
